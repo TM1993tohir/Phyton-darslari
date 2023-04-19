39b2ac39d5d983de439813f5490898de23997fb9
Tohirjon <131234096+TM1993tohir@users.noreply.github.com>
2023-04-19 12:01:16 +0500

    YANGI DARS

---
+++ Royhatni_tartiblash.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 11:20:24 2023
+
+@author: t.mamasoliyev
+"""
+
+                            #Ro'yxatni tartiblash   
+                            
+#cars=['mers', 'audi', 'gm', 'shevrolet', 'alfarameo', 'ferrari', 'Caddelac']
+#print(cars)
+#cars.sort()
+#print(cars)
+
+
+
+
+#Teskari tartiblash
+#cars=['mers', 'audi', 'gm', 'shevrolet', 'alfarameo', 'ferrari', 'Caddelac']
+#print(cars)
+
+#cars.sort(reverse=True)
+#print(cars)
+
+#cars=['mers', 'audi', 'gm', 'shevrolet', 'alfarameo', 'ferrari', 'Caddelac']
+#print(sorted(cars))
+#print(sorted(cars, reverse=True))
+
+#cars=['mers', 'audi', 'gm', 'shevrolet', 'alfarameo', 'ferrari', 'Caddelac']
+#cars.reverse()
+#print(cars)
+#uzunlik=len(cars)
+#print(uzunlik)
+                                #Ro'yxatni kesib olish
+#raqamlar=list(range(0,10))
+#print(raqamlar)
+#toq_sonlar=list(range(1,10,2))
+#print(toq_sonlar)
+#max_qiymat=max(toq_sonlar)
+#print(max_qiymat)
+#min_qiymat=min(toq_sonlar)
+#print(min_qiymat)
+#toq_sonlar_yigindisi=sum(toq_sonlar)
+#print(toq_sonlar_yigindisi)
+#print(toq_sonlar[0:5])
+#print(toq_sonlar[0:])
+#print(toq_sonlar[:3])
+
+                            #Ro'yxatdan nusxa olish          
+#sonlar=[1,2,3,4,5,6,7,8,9]
+#sonlar_2=sonlar[:]
+#print(sonlar)
+#print(sonlar_2)
+#sonlar_2.append(12)
+#print(sonlar_2)
+#print(sonlar)
+
+                                            #AMALIYOT
+                                            #String tipida amallar
+
+davlatlar=['Singapure', 'Buyuk Britanya', 'Uzbekistan', 'Ispanya', 'Georgiya']
+#print(davlatlar)
+#uzunlik=len(davlatlar)
+#print(uzunlik)
+#print(sorted(davlatlar))
+#print(sorted(davlatlar, reverse=True))
+#print(davlatlar)
+#davlatlar.reverse()
+#print(davlatlar)
+#davlatlar.sort()
+#davlatlar.sort(reverse=True)
+#print(davlatlar)
+
+                             #Sonlar bilan amallar
+#sonlar=list(range(1,1201))
+#print(sonlar)
+#juft_sonlar=list(range(120,1200,2))
+#print(juft_sonlar)
+#yigindi=sum(sonlar)
+#max_son=max(sonlar)
+#min_son=min(sonlar)
+#print(max_son-min_son)
+#e_soni=len(sonlar)
+#print(e_soni)
+#print(sonlar[0:20], sonlar[599:620], sonlar[1179:1200])
+
+                                               #Taomlar
+#taomlar=['palov', 'shorva', 'mastava', 'mampar','chuchvara']
+#nonushta=taomlar[:]
+#print(nonushta)
+#del(nonushta[0])
+#del(nonushta[2])
+#del(nonushta[2])
+#nonushta.append('kasha')
+#nonushta.append('grichka')
+#print(nonushta)
+#print(taomlar)
+#nonushta=tuple(nonushta)
+#nonushta[0]='qaymoq va non'
+#print(nonushta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sonlar USTIDA AMALLAR.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 12:22:14 2023
+
+@author: t.mamasoliyev
+"""
+
+#Sonlar ustida ishlash
+
+
+
+#a=5
+#b=4.5
+#aholi_soni=7_595_315_885
+#print("Dunyodagi aholi soni: ", aholi_soni)
+#x, y, z =10,15,2.5
+#print(x,y,z)
+
+
+
+#t_yil=int(input("Tug'ilgan yilingizni kiriting: "))
+#yosh=2023-t_yil
+#print("Siz " ,yosh, " yoshdasiz")
+
+                                     #Amaliyot
+
+#a=int(input("Istalgan sonni kiriting \n >>> "))
+#c=a**2
+#d=a**3
+#print(a, "ning kvadrati" , c, "ga teng")
+#print(a," ning kubi ", d, " ga teng")
+
+#yosh=int(input("Yoshingiz nechida \n >>>"))
+#t_yil=2023-yosh
+#print("Demak siz ", t_yil,"-yilda tug'ilgansiz")
+
+
+#a=float(input("birinchi sonni kiriting: "))
+#b=float(input("ikkinchi sonni kiriting: "))
+#print(a, "+", b, "=", a+b)
+#print(a, "-", b, "=", a-b)
+#print(a, "*", b, "=", a*b)
+#print(a, "/", b, "=", a/b)

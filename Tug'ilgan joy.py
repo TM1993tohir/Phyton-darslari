4b113e0d0c2ff74e0786144388e6a005fdce3b47
Tohirjon <131234096+TM1993tohir@users.noreply.github.com>
2023-04-19 12:06:06 +0500

    yangi darslar

---
+++ Tug'ilgan joy.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Matn metodlari
+
+#ism='Tohirjon'
+#familiya='Mamasoliyev'
+#ism_familiya=f"{ism} {familiya}"
+#print(ism_familiya.capitalize())
+
+
+#meva="   olmani   "
+#print("Men" + " " + meva.lstrip() + "yaxshi ko'raman" )
+#print("Men" + " " + meva.rstrip() + " " + "yaxshi ko'raman")
+#print("Men" + " " + meva.strip() + " " + "yaxshi ko'raman")
+
+#input funksiyasi
+
+#ism=input('Ismingiz nima ?' )  
+#print("Assalomu alaykum" + " " + ism)
+   
+#ism=input("Ismingiz nima \n>>>")
+#print("Assalomu alaykum, " + ism.title())
+
+#Amaliyot
+
+#kocha="Bog'bon"
+#mahalla="Sog'bon"
+#tuman="Bodomzor"
+#viloyat="Samarqand"
+#toliq=f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+#print(toliq)
+
+#kocha= input('ko\'changizni kiriting ' )
+#mahalla=input("mahallangizni kiriting " )
+#tuman=input("Tumaningizni kiriting ")
+#viloyat=input("viloyatingizni kiriting ")
+#toliq=f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+#print(toliq)
+#print(kocha+" ko'chasi")
+#print(mahalla + " mahallasi")
+#print(tuman + " tumani")
+#print(viloyat + " viloyati")
+
+kocha= input('ko\'changizni kiriting ' )
+mahalla=input("mahallangizni kiriting " )
+tuman=input("Tumaningizni kiriting ")
+viloyat=input("viloyatingizni kiriting ")
+toliq=f"{kocha} ko'chasi,\n {mahalla} mahallasi,\n {tuman} tumani,\n {viloyat} viloyati"
+print(toliq)
+print(kocha+" ko'chasi".upper())
+print(mahalla + " mahallasi".title())
+print(tuman + " tumani".capitalize())
+print(viloyat + " viloyati".lower())
+
+
+
+  
---
+++ tem bilan ishlar.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+#print((2+8)*9)
+#radius = 5
+#pi = 3.14159
+#aylana_yuzi = pi * radius**2
+#print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+#Matn metodlari
+
+#ism = "Tohirjon"
+#print("mening ismim" + " " + ism)
+#smile ="♡"
+
+#ism ='Tohirjon'
+#familiya='Mamasoliyev'
+#ism_familiya=f'{ism} {familiya}'
+#print(ism_familiya)
+
+#ism='Tohirjon'
+#familiya='Mamasoliyev'
+#print(f"Salom, mening ismim {ism}. {ism} {familiya} !")
+
+#ism='Tohirjon'
+#familiya='Mamasoliyev'
+#ism_familiya=f"{ism} {familiya}"
+#print(ism_familiya.capitalize())
+
+
+#meva="   olmani   "
+#print("Men" + " " + meva.lstrip() + "yaxshi ko'raman" )
+#print("Men" + " " + meva.rstrip() + " " + "yaxshi ko'raman")
+#print("Men" + " " + meva.strip() + " " + "yaxshi ko'raman")
+
+#input funksiyasi
+
+#ism=input('Ismingiz nima ?' )  
+#print("Assalomu alaykum" + " " + ism)
+   
+#ism=input("Ismingiz nima \n>>>")
+#print("Assalomu alaykum, " + ism.title())

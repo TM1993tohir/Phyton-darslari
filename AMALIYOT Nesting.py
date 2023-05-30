9daea56953926b723348c163852305efe4cd8d45
Tohirjon <131234096+TM1993tohir@users.noreply.github.com>
2023-05-30 15:13:50 +0500

    Asosiy

--- AMALIYOT Nesting.py
+++ AMALIYOT Nesting.py
@@ -6,7 +6,7 @@ Created on Thu May 25 10:11:32 2023
 """
 #Amaliyot:Nesting
 #Muallif:Tohirjon Mamasoliyev 
-alloma0={ 'ism':'Abu Abdulloh Muhammad ibn Ismoil ibn Ibrohim al Buxoriy',
+alloma0 = { 'ism':'Abu Abdulloh Muhammad ibn Ismoil ibn Ibrohim al Buxoriy',
         'tyil':810,
         'tjoy':'Buxoro',
         'unvoni':' Muhaddislar imomi, hadis ilmining sultoni',
@@ -14,36 +14,86 @@ alloma0={ 'ism':'Abu Abdulloh Muhammad ibn Ismoil ibn Ibrohim al Buxoriy',
         'asar':['Al-jomeʼ as-sahih', 'At-tarix al-kabir', 'At-tarix al-avsat']
         }
 alloma1={ 'ism':'Abul Abbos Ahmad ibn Muhammad ibn Kasir al-Fargʻoniy',
-         'tyil':797,
-         'tjoy':'Fargona',
-         'unvoni':'oʻzbek qomusiy olimi',
-         'vafoti':865,
-         'asar':['Kitob al-harakot as-samoviya va javomi ilm an-nujum', ]
+          'tyil':797,
+          'tjoy':'Fargona',
+          'unvoni':'oʻzbek qomusiy olimi',
+          'vafoti':865,
+          'asar':['Kitob al-harakot as-samoviya va javomi ilm an-nujum', ]
     }
 alloma2={'ism':'Alisher Navoiy',
-         'tyil':1441,
-         'tjoy':'Hirot',
-         'unvoni':'gazal mulkining sultoni',
-         'vafoti':1501,
-         'asar':['Majolisun-nafois', 'Mahbubul-qulub', 'Lisonut-tayr']
-    }
+          'tyil':1441,
+          'tjoy':'Hirot',
+          'unvoni':'gazal mulkining sultoni',
+          'vafoti':1501,
+          'asar':['Majolisun-nafois', 'Mahbubul-qulub', 'Lisonut-tayr']
+     }
+
+
 
 olimlar=[alloma0, alloma1, alloma2]
 
-# for olim in olimlar:
-       # print(f"{olim['ism'].title()}",
-       #         f"{olim['tyil']}-yilda",
-       #         f"{olim['tjoy']}da tug\'ilgan,",
-       #         f"{olim['unvoni']} buyuk alloma ",
-       #         f"{olim['vafoti']}-yilda vafot etgan")
+# for shax in olimlar:
+#     print(shax['ism'])
+#     asr =  shax['asar']
+#     for asa in asr:
+#         print(asr)
 
-    # print(f"{olim['ism'].title()}ning mashxur asarlari")
-    # for kitob in info['asar']:
-    #     print(f"{kitob.upper()}")
 
-for shaxs in olimlar:
-    ism = shaxs['ism']
-    asarlar = shaxs['asar']
-    print(f"\n{ism} ning mashxur asarlari: ")
-    for asar in asarlar:
-        print(asar)
+for olim in olimlar:
+        print(f"{olim['ism'].title()}",
+                f"{olim['tyil']}-yilda",
+                f"{olim['tjoy']}da tug\'ilgan,",
+                f"{olim['unvoni']} buyuk alloma ",
+                f"{olim['vafoti']}-yilda vafot etgan")
+  
+
+# for shaxs in olimlar:
+#     ism = shaxs['ism']
+#     asarlar = shaxs['asar']
+#     print(f"\n{ism} ning mashxur asarlari: ")
+#     for asar in asarlar:
+#         print(asar)\
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
---
+++ AMALIYOT.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 10:11:32 2023
+
+@author: t.mamasoliyev
+"""
+#Amaliyot:Nesting
+#Muallif:Tohirjon Mamasoliyev 
+alloma0={ 'ism':'Abu Abdulloh Muhammad ibn Ismoil ibn Ibrohim al Buxoriy',
+        'tyil':810,
+        'tjoy':'Buxoro',
+        'unvoni':' Muhaddislar imomi, hadis ilmining sultoni',
+        'vafoti':870,
+        'asar':['Al-jomeʼ as-sahih', 'At-tarix al-kabir', 'At-tarix al-avsat']
+        }
+alloma1={ 'ism':'Abul Abbos Ahmad ibn Muhammad ibn Kasir al-Fargʻoniy',
+         'tyil':797,
+         'tjoy':'Fargona',
+         'unvoni':'oʻzbek qomusiy olimi',
+         'vafoti':865,
+         'asar':['Kitob al-harakot as-samoviya va javomi ilm an-nujum', ]
+    }
+alloma2={'ism':'Alisher Navoiy',
+         'tyil':1441,
+         'tjoy':'Hirot',
+         'unvoni':'gazal mulkining sultoni',
+         'vafoti':1501,
+         'asar':['Majolisun-nafois', 'Mahbubul-qulub', 'Lisonut-tayr']
+    }
+
+olimlar=[alloma0, alloma1, alloma2]
+
+# for olim in olimlar:
+       # print(f"{olim['ism'].title()}",
+       #         f"{olim['tyil']}-yilda",
+       #         f"{olim['tjoy']}da tug\'ilgan,",
+       #         f"{olim['unvoni']} buyuk alloma ",
+       #         f"{olim['vafoti']}-yilda vafot etgan")
+
+    # print(f"{olim['ism'].title()}ning mashxur asarlari")
+    # for kitob in info['asar']:
+    #     print(f"{kitob.upper()}")
+
+for shaxs in olimlar:
+    ism = shaxs['ism']
+    asarlar = shaxs['asar']
+    print(f"\n{ism} ning mashxur asarlari: ")
+    for asar in asarlar:
+        print(asar)
